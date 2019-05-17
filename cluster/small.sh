ce2ed2c4de14ceb80655f69fd0e048405cddae58
Michele Sciabarra <msciabarra@apache.org>
2019-05-17 09:35:38 +0200

    temp yaml

--- cluster/small.sh
+++ cluster/small.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-YAML=${1:-cloud-init.yaml}
+YAML=${1:-cloud-init$$.yaml}
 if ! test -f $YAML
 then curl -s http://kubepass.sciabarra.com >$YAML
 fi
 multipass launch -n kube-master -c 2 -m 2G -d 10G --cloud-init $YAML
 multipass launch -n kube-worker1 -m 1G -d 10G --cloud-init $YAML
 multipass launch -n kube-worker2 -m 1G -d 10G --cloud-init $YAML
+rm $YAML
 multipass exec -- kube-master bash -c "cloud-init status --wait && sudo watch kubectl get nodes"
