ee19cfb3c825fdd653a389c42567ce56699b0928
Michele Sciabarra <msciabarra@apache.org>
2019-05-17 09:48:00 +0200

    missing -L

--- cluster/large.sh
+++ cluster/large.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 YAML=${1:-cloud-init$$.yaml}
 if ! test -f $YAML
-then curl -s http://kubepass.sciabarra.com >$YAML
+then curl -Ls http://kubepass.sciabarra.com >$YAML
 fi
 multipass launch -n kube-master -c 2 -m 4G -d 25G --cloud-init $YAML
 multipass launch -n kube-worker1 -c 2-m 4G -d 25G --cloud-init $YAML
--- cluster/small.sh
+++ cluster/small.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 YAML=${1:-cloud-init$$.yaml}
 if ! test -f $YAML
-then curl -s http://kubepass.sciabarra.com >$YAML
+then curl -Ls http://kubepass.sciabarra.com >$YAML
 fi
 multipass launch -n kube-master -c 2 -m 2G -d 10G --cloud-init $YAML
 multipass launch -n kube-worker1 -m 1G -d 10G --cloud-init $YAML
