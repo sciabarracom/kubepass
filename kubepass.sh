fd803b3dce8df26ba3df2f55dcf532a646dab185
Michele Sciabarra <msciabarra@apache.org>
2019-05-18 22:52:24 +0200

    fix to the temp file

--- kubepass.sh
+++ kubepass.sh
@@ -4,13 +4,16 @@
 CMD="${1:-small}"
 NUM="${2:-3}"
 
+TEMP=/tmp
 YAML=/tmp/kubepass.yaml
 MULTIPASS=multipass
+
 WINMULTIPASS="/c/Program Files/Multipass"
 if test -d "$WINMULTIPASS"
 then PATH="$WINMULTIPASS/bin:$PATH"
      MULTIPASS=multipass.exe
-     YAML=/c/Windows/Temp/kubepass.yaml
+     TEMP=/c/Windows/Temp
+     YAML=c:/Windows/Temp/kubepass.yaml
 fi
 if ! "$MULTIPASS" -h >/dev/null 
 then echo "Install multipass 0.6.1, please."
@@ -22,7 +25,7 @@ build() {
    COUNT="$1"
    ARGS_MASTER="$2"
    ARGS_WORKERS="$3"
-   test -f $YAML || curl -s https://kubepass.sciabarra.com/kubepass.yaml >$YAML
+   test -f $YAML || curl -s https://kubepass.sciabarra.com/kubepass.yaml >$TEMP/kubepass.yaml
    "$MULTIPASS" launch -n kube-master $ARGS_MASTER --cloud-init $YAML
    for (( I=1 ; I<= $COUNT; I++))
    do "$MULTIPASS" launch -n "kube-worker$I" $ARGS_WORKERS --cloud-init $YAML
